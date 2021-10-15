8ee435db0a4d374ec08ba79c18456132befcb809
AyushGothi123 <72344991+AyushGothi123@users.noreply.github.com>
2021-10-15 12:42:37 +0530

    Add files via upload

---
+++ sd.py
@@ -0,0 +1,43 @@
+import csv
+import math
+import statistics
+with open("data.csv",newline = "") as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+
+
+
+
+data=  file_data[0]
+def mean(data):
+    n = len(data)
+    total = 0
+    for i in data:
+        total += int(i)
+        
+
+    mean = total/n
+    return mean
+
+
+squared_list = []
+for number in data:
+    a = int(number) - mean(data)
+    a  = a**2
+    squared_list.append(a)
+
+sum = 0
+for i in squared_list:
+    sum += i
+
+result = sum/(len(data)-1)
+standardDeviation = math.sqrt(result)
+print(standardDeviation)
+
+yellow=[60,61,65,63,98,99,90,95,91,96]
+print(statistics.stdev(yellow))
+print(statistics.mean(yellow))
+
+
+    
